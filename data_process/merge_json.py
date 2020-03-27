c459ffff05a7ee5bc5a3f9e96a829ea2efa3f571
Qianying Wang <w.qianying@outlook.com>
2020-03-27 21:44:52 +0000

    error checking

--- data_process/merge_json.py
+++ data_process/merge_json.py
@@ -12,7 +12,19 @@ import json
 import pandas as pd
 import random
 import re
+import pickle
+from tqdm import tqdm
 random.seed(1234)
+
+os.chdir('/home/qwang/rob/rob-pome')
+from data_process.regex import doc_annotate, read_regex
+os.chdir('/home/qwang/rob')
+regex_random = read_regex('regex/RandomizationTreatmentControl.txt')
+regex_blind = read_regex('regex/BlindedOutcomeAssessment.txt')
+regex_sample = read_regex('regex/SampleSizeCalculation.txt')
+regex_conflict = read_regex('regex/ConflictsOfInterest.txt')
+regex_welfare = read_regex('regex/AnimalWelfareRegulations.txt')
+
 os.chdir('/media/mynewdrive/rob/data')
 
 
@@ -115,7 +127,8 @@ for g in gold_final:
 gold_df.to_csv('rob_gold_info.txt', sep='\t', encoding='utf-8')
         
    
-#%% Generate error checking records
+#%% Error correction - 1
+# Generate error checking records for random/blinded/samplesize (randomly 5% from each project)
 g_stroke = [g for g in gold if re.sub(r'[0-9]', '', g['goldID']) == 'stroke'] 
 g_np = [g for g in gold if re.sub(r'[0-9]', '', g['goldID']) == 'np'] 
 g_psy = [g for g in gold if re.sub(r'[0-9]', '', g['goldID']) == 'psy'] 
@@ -135,7 +148,7 @@ gold_err = g_stroke[:int(len(g_stroke)*0.05)] + \
            g_iicarus[:int(len(g_iicarus)*0.05)]
            
            
-# Create data frame for error checking           
+# Create data frame for SyRF          
 err_df = pd.DataFrame(columns = ['Title', 'Authors', 'Publication Name', 'Alternate Name', 'Abstract',
                                  'Url', 'Author Address', 'Year', 'DOI', 
                                  'Keywords', 'Reference Type', 'PDF Relative Path'])
@@ -174,9 +187,191 @@ for i, row in err_df.iterrows():
 
 err_df.to_csv('data/error_review/rob_error_checking.txt', sep='\t', encoding='utf-8')
 
+
+
+#%% Error correction - 2
+# Compare annotations for error_review (from MM)
+err = pd.read_csv("error_review/rob_error_checking.txt", sep='\t', engine="python", encoding="utf-8")   
+err = err[['Unnamed: 0', 'Keywords', 'RandomizationTreatmentControl', 'BlindedOutcomeAssessment', 'SampleSizeCalculation', 'PDF Relative Path']]
+err.rename(columns={'Unnamed: 0': 'ID', 'Keywords': 'goldID'}, inplace=True)
+
+
+mal = pd.read_csv("error_review/rob_annotation_malcolm.csv", sep=',', engine="python", encoding="utf-8")
+mal = mal[['Title', 'Randomisation', 'Blinding', 'SampleSizeCalculation', 'StudyId']]
+mal.rename(columns={'Title': 'ID', 'Randomisation': 'RandomizationTreatmentControl', 'Blinding': 'BlindedOutcomeAssessment'}, inplace=True)
+mal = mal.replace({True: 1, False: 0})
+
+res = pd.merge(mal, err, how='left', on='ID')
+
+for i in range(len(res)):
+    res.loc[i, 'group'] = re.sub(r'\d+', "", res.loc[i, 'goldID'])
+
+
+df = res[res.group == 'psy']
+df.set_index(pd.Series(range(0, len(df))), inplace=True)
+tr = df[df.RandomizationTreatmentControl_y == df.RandomizationTreatmentControl_x]
+tb = df[df.BlindedOutcomeAssessment_y == df.BlindedOutcomeAssessment_x]
+ts = df[df.SampleSizeCalculation_y == df.SampleSizeCalculation_x]
+print(round(len(tr)/len(df)*100,2), round(len(tb)/len(df)*100,2), round(len(ts)/len(df)*100,2))
+
+# Output inconsistent records between gold data and Malcolm's annotation
+df = res
+df_mis = df[(df.RandomizationTreatmentControl_y != df.RandomizationTreatmentControl_x) | 
+            (df.BlindedOutcomeAssessment_y != df.BlindedOutcomeAssessment_x) |
+            (df.SampleSizeCalculation_y != df.SampleSizeCalculation_x)]
+df_mis.rename(columns={'RandomizationTreatmentControl_x': 'Randomization_M', 
+                       'BlindedOutcomeAssessment_x': 'Blinded_M', 
+                       'SampleSizeCalculation_x': 'SSC_M',
+                       'RandomizationTreatmentControl_y': 'Randomization_G', 
+                       'BlindedOutcomeAssessment_y': 'Blinded_G', 
+                       'SampleSizeCalculation_y': 'SSC_G'                     
+                       }, inplace=True)
+
+df_mis.to_csv('error_review/rob_error_inconsistent.csv', sep=',', encoding='utf-8')
+
+
+# Generate error checking records for exclusion/welfare/conceal/conflict
+df_info = pd.read_pickle('rob_info_a.pkl') 
+df_info.rename(columns={'goldID': 'Keywords'}, inplace=True)
+df_err = pd.read_csv('error_review/rob_error_checking.txt', sep='\t', encoding='utf-8')
+df_err.rename(columns={'Unnamed: 0': 'ID'}, inplace=True)
+
+res = pd.merge(df_err, df_info, how='left', on='Keywords')
+res = res[[  'ID',
+             'Title',
+             'Authors',
+             'Publication Name',
+             'Alternate Name',
+             'Abstract',
+             'Url',
+             'Author Address',
+             'Year',
+             'DOI',
+             'Keywords',
+             'Reference Type',
+             'PDF Relative Path',
+             'RandomizationTreatmentControl_y',
+             'AllocationConcealment_y',
+             'BlindedOutcomeAssessment_y',
+             'SampleSizeCalculation_y',
+             'AnimalWelfareRegulations_y',
+             'ConflictsOfInterest_y',
+             'AnimalExclusions_y',
+             'partition']]
+
+res.rename(columns={'RandomizationTreatmentControl_y': 'RandomizationTreatmentControl', 
+                    'BlindedOutcomeAssessment_y': 'BlindedOutcomeAssessment', 
+                    'SampleSizeCalculation_y': 'SampleSizeCalculation',
+                    'AllocationConcealment_y': 'AllocationConcealment', 
+                    'AnimalWelfareRegulations_y': 'AnimalWelfareRegulations', 
+                    'ConflictsOfInterest_y': 'ConflictsOfInterest',
+                    'AnimalExclusions_y': 'AnimalExclusions'                      
+                    }, inplace=True)
+
+res.to_csv('error_review/rob_error_check_update.txt', sep='\t', encoding='utf-8')
+
+#%% Error correction - 3 
+## Error checking for gold data (from regex)
+err = pd.read_csv("error_review/rob_error_checking.txt", sep='\t', engine="python", encoding="utf-8")  
+id_checked = list(err['Keywords'])  #ids of records checked by MM
+
+# Get regex label
+df = pd.read_pickle('rob_info_a.pkl')  # for random/blind/sample
+for i, row in tqdm(df.iterrows()): 
+    # Read string list from pkl file
+    pkl_path = os.path.join('rob_str', df.loc[i,'goldID']+'.pkl') 
+    with open(pkl_path, 'rb') as fin:
+        sent_ls = pickle.load(fin)        
+    # Extract text    
+    text = ''
+    for l in sent_ls:
+        t = ' '.join(l)
+        text = text + t       
+    df.loc[i,'rgx_random'] = doc_annotate(regex_random, text)
+    df.loc[i,'rgx_blind'] = doc_annotate(regex_blind, text)
+    df.loc[i,'rgx_sample'] = doc_annotate(regex_sample, text)
+    df.loc[i,'rgx_conflict'] = doc_annotate(regex_conflict, text)
+    df.loc[i,'rgx_welfare'] = doc_annotate(regex_welfare, text)
+
+list(df.columns)
+for i in range(len(df)):
+    df.loc[i, 'project'] = re.sub(r'\d+', "", df.loc[i, 'goldID'])
+
+
+# Mismatch
+sub = df
+#sub = df[df.project == 'iicarus']
+#df_mis = sub[sub.rgx_random != sub.RandomizationTreatmentControl]; print(len(df_mis)) 
+#df_mis = sub[sub.rgx_blind != sub.BlindedOutcomeAssessment]; print(len(df_mis)) 
+#df_mis = sub[sub.rgx_sample != sub.SampleSizeCalculation]; print(len(df_mis)) 
+#df_mis = sub[(sub['ConflictsOfInterest'].isnull() == False) & (sub.rgx_conflict != sub.ConflictsOfInterest)]; print(len(df_mis)) 
+#df_mis = sub[(sub['AnimalWelfareRegulations'].isnull() == False) & (sub.rgx_welfare != sub.AnimalWelfareRegulations)]; print(len(df_mis)) 
+#
+#df_mis = sub[(sub.rgx_random != sub.RandomizationTreatmentControl) | 
+#             (sub.rgx_blind != sub.BlindedOutcomeAssessment) |
+#             (sub.rgx_sample != sub.SampleSizeCalculation) |
+#             ((sub['ConflictsOfInterest'].isnull() == False) & (sub.rgx_conflict != sub.ConflictsOfInterest)) |
+#             ((sub['AnimalWelfareRegulations'].isnull() == False) & (sub.rgx_welfare != sub.AnimalWelfareRegulations))]
+
+# rand/blind/ssc mismatched at the same time
+df_mis = sub[(sub.rgx_random != sub.RandomizationTreatmentControl) & 
+             (sub.rgx_blind != sub.BlindedOutcomeAssessment) &
+             (sub.rgx_sample != sub.SampleSizeCalculation)]
+
+# Every two of rand/blind/ssc mismatched
+df_mis = sub[((sub.rgx_random != sub.RandomizationTreatmentControl) & (sub.rgx_blind != sub.BlindedOutcomeAssessment)) |
+             ((sub.rgx_random != sub.RandomizationTreatmentControl) & (sub.rgx_sample != sub.SampleSizeCalculation)) |
+             ((sub.rgx_blind != sub.BlindedOutcomeAssessment) & (sub.rgx_sample != sub.SampleSizeCalculation))]
+
+ids = list(df_mis['goldID'])
+id_common = list(set(id_checked) & set(ids))  # len = 24
+# Removed records checked by MM
+df_fin = df_mis[-df_mis["goldID"].isin(id_common)]  # len = 496-24 = 472
+
+# Info list of all gold data
+gold_info = read_json(json_path='rob_info.json')
+err_list = [g for g in gold_info if g['goldID'] in list(df_fin['goldID'])]
+
+
+# Create data frame for SyRF          
+err_df = pd.DataFrame(columns = ['Title', 'Authors', 'Publication Name', 'Alternate Name', 'Abstract',
+                                 'Url', 'Author Address', 'Year', 'DOI', 
+                                 'Keywords', 'Reference Type', 'PDF Relative Path'])
+
+for g in err_list:
+    err_df = err_df.append({'Title': "", 'Authors': "", 'Publication Name': "", 'Alternate Name': "", 'Abstract': "",
+                            'Url': "", 'Author Address': "", 'Year': "", 'DOI': "", 
+                            'Keywords': g['goldID'], 'Reference Type': g['goldID'], 'PDF Relative Path': g['DocumentLink'],
+                            'RandomizationTreatmentControl': g['RandomizationTreatmentControl'],
+                            'AllocationConcealment': g['AllocationConcealment'],
+                            'BlindedOutcomeAssessment': g['BlindedOutcomeAssessment'],
+                            'SampleSizeCalculation': g['SampleSizeCalculation'],
+                            'AnimalWelfareRegulations': g['AnimalWelfareRegulations'],
+                            'ConflictsOfInterest': g['ConflictsOfInterest'],
+                            'AnimalExclusions': g['AnimalExclusions']
+                            }, ignore_index=True)
     
+    
+# Modify Reference type
+err_df['Reference Type'] = err_df['Reference Type'].str.replace(r'[0-9]', '')  
+
+# Copy PDFs to 'error_review' folder
+err_df['PDF Relative Path'] = err_df['PDF Relative Path'].str.replace('psycho/', "data/psycho/")
+
+os.chdir('/media/mynewdrive/rob')
+for i, row in err_df.iterrows():  
+    old_path = err_df.loc[i,'PDF Relative Path']            
+    new_path = os.path.join('data/error_review', err_df.loc[i,'Reference Type'] , os.path.basename(old_path))
+    err_df.loc[i,'PDF Relative Path'] = new_path    
+    if os.path.exists(old_path) == False:
+        print('PDF does not exist: {}'.format(old_path))
+    else:        
+        shutil.copy2(old_path, new_path)
 
 
+err_df.to_csv('data/error_review/rob_error_check_regex.csv', sep=',', encoding='utf-8')
 
+
+    
 
 
--- data_process/regex.py
+++ data_process/regex.py
@@ -7,12 +7,7 @@ Created on Wed Oct  9 16:28:49 2019
 """
 
 import re
-import pickle
-import pandas as pd
-import os
-from tqdm import tqdm
-data_dir = '/media/mynewdrive/rob/'
-os.chdir(data_dir)
+
 
 #%%
 # Read regex string
@@ -20,12 +15,6 @@ def read_regex(regex_path):
     with open(regex_path, "r", encoding='utf-8') as fin:
         regex = fin.read()
     return regex
-
-regex_random = read_regex('regex/RandomizationTreatmentControl.txt')
-regex_blind = read_regex('regex/BlindedOutcomeAssessment.txt')
-regex_sample = read_regex('regex/SampleSizeCalculation.txt')
-regex_conflict = read_regex('regex/ConflictsOfInterest.txt')
-regex_welfare = read_regex('regex/AnimalWelfareRegulations.txt')
 
 
 # Document annotation
@@ -59,43 +48,6 @@ def doc_annotate(regex, doc):
 #sent_labeled[0][0] # sentence
 #sent_labeled[0][1] # label
 
-#%% Error checking for gold data
-df = pd.read_pickle('data/rob_info_a.pkl')  # for random/blind/sample
- 
-# Get regex label
-for i, row in tqdm(df.iterrows()): 
-    # Read string list from pkl file
-    pkl_path = os.path.join('data/rob_str', df.loc[i,'goldID']+'.pkl') 
-    with open(pkl_path, 'rb') as fin:
-        sent_ls = pickle.load(fin)
-        
-    # Extract text    
-    text = ''
-    for l in sent_ls:
-        t = ' '.join(l)
-        text = text + t       
-    df.loc[i,'rgx_random'] = doc_annotate(regex_random, text)
-    df.loc[i,'rgx_blind'] = doc_annotate(regex_blind, text)
-    df.loc[i,'rgx_sample'] = doc_annotate(regex_sample, text)
-    df.loc[i,'rgx_conflict'] = doc_annotate(regex_conflict, text)
-    df.loc[i,'rgx_welfare'] = doc_annotate(regex_welfare, text)
 
-list(df.columns)
 
-# Mismatch
-for i in range(len(df)):
-    df.loc[i, 'project'] = re.sub(r'\d+', "", df.loc[i, 'goldID'])
 
-sub = df
-sub = df[df.project == 'iicarus']
-df_mis = sub[sub.rgx_random != sub.RandomizationTreatmentControl]; print(len(df_mis)) 
-df_mis = sub[sub.rgx_blind != sub.BlindedOutcomeAssessment]; print(len(df_mis)) 
-df_mis = sub[sub.rgx_sample != sub.SampleSizeCalculation]; print(len(df_mis)) 
-df_mis = sub[(sub['ConflictsOfInterest'].isnull() == False) & (sub.rgx_conflict != sub.ConflictsOfInterest)]; print(len(df_mis)) 
-df_mis = sub[(sub['AnimalWelfareRegulations'].isnull() == False) & (sub.rgx_welfare != sub.AnimalWelfareRegulations)]; print(len(df_mis)) 
-
-df_mis = sub[(sub.rgx_random != sub.RandomizationTreatmentControl) | 
-             (sub.rgx_blind != sub.BlindedOutcomeAssessment) |
-             (sub.rgx_sample != sub.SampleSizeCalculation) |
-             ((sub['ConflictsOfInterest'].isnull() == False) & (sub.rgx_conflict != sub.ConflictsOfInterest)) |
-             ((sub['AnimalWelfareRegulations'].isnull() == False) & (sub.rgx_welfare != sub.AnimalWelfareRegulations))]
