bafbd38dd069871f250089c3ec3cce79c73233f6
Qianying Wang <w.qianying@outlook.com>
2020-11-09 15:45:04 +0000

    csv i0

--- rob-app/api.py
+++ rob-app/api.py
@@ -10,55 +10,27 @@ from flask import Flask, request
 from flask_restful import Resource, Api
 import os
 
-import sys
+
 import re
 import json
-from pred import pred_prob, pred_prob_bert, extract_sents
+import pandas as pd
+from pred_fn import pred_prob, pred_prob_distil, extract_sents
         
              
 #%%
 PROB_PATH = {
-    'arg-r': 'pth/awr_13.json',
-    'pth-r': 'pth/awr_13.pth.tar',
-    'fld-r': 'fld/awr_13.Field',
-    
-    'arg-b': 'pth/awb_32.json',
-    'pth-b': 'pth/awb_32.pth.tar',
-    'fld-b': 'fld/awb_32.Field',
-    
-    'arg-i': 'pth/cwi_6.json',
-    'pth-i': 'pth/cwi_6.pth.tar',
-    'fld-i': 'fld/cwi_6.Field',
-    
-    'arg-w': 'pth/bert_w0.json',
-    'pth-w': 'pth/bert_w0.pth.tar',
-    # 'wgt-w': 'pth/biobert',
-    
-    'arg-e': 'pth/awe_8.json',
-    'pth-e': 'pth/awe_8.pth.tar',
-    'fld-e': 'fld/awe_8.Field',
+    'arg-r': 'pth/awr_13.json', 'pth-r': 'pth/awr_13.pth.tar', 'fld-r': 'pth/awr_13.Field',  
+    'arg-b': 'pth/awb_32.json', 'pth-b': 'pth/awb_32.pth.tar', 'fld-b': 'pth/awb_32.Field',   
+    'arg-i': 'pth/cwi_6.json', 'pth-i': 'pth/cwi_6.pth.tar', 'fld-i': 'pth/cwi_6.Field',
+    'arg-w': 'pth/dsc_w0.json', 'pth-w': 'pth/dsc_w0.pth.tar', 
+    'arg-e': 'pth/awe_8.json', 'pth-e': 'pth/awe_8.pth.tar', 'fld-e': 'pth/awe_8.Field',
 }
-
 SENT_PATH = {
-    'arg-r': 'pth/hr_4.json',
-    'pth-r': 'pth/hr_4.pth.tar',
-    'fld-r': 'fld/hr_4.Field',
-    
-    'arg-b': 'pth/hb_5.json',
-    'pth-b': 'pth/hb_5.pth.tar',
-    'fld-b': 'fld/hb_5.Field',
-    
-    'arg-i': 'pth/hi_4.json',
-    'pth-i': 'pth/hi_4.pth.tar',
-    'fld-i': 'fld/hi_4.Field',
-    
-    'arg-w': 'pth/hw_17.json',
-    'pth-w': 'pth/hw_17.pth.tar',
-    'fld-w': 'fld/hw_17.Field',
-    
-    'arg-e': 'pth/he_26.json',
-    'pth-e': 'pth/he_26.pth.tar',
-    'fld-e': 'fld/he_26.Field'
+    'arg-r': 'pth/hr_4.json', 'pth-r': 'pth/hr_4.pth.tar', 'fld-r': 'pth/hr_4.Field',
+    'arg-b': 'pth/hb_5.json', 'pth-b': 'pth/hb_5.pth.tar', 'fld-b': 'pth/hb_5.Field',    
+    'arg-i': 'pth/hi_4.json', 'pth-i': 'pth/hi_4.pth.tar', 'fld-i': 'pth/hi_4.Field', 
+    'arg-w': 'pth/hw_17.json', 'pth-w': 'pth/hw_17.pth.tar', 'fld-w': 'pth/hw_17.Field',  
+    'arg-e': 'pth/he_26.json', 'pth-e': 'pth/he_26.pth.tar', 'fld-e': 'pth/he_26.Field'
 }
 
 p_ref = re.compile(r"(.*Reference\s{0,}\n)|(.*References\s{0,}\n)|(.*Reference list\s{0,}\n)|(.*REFERENCE\s{0,}\n)|(.*REFERENCES\s{0,}\n)|(.*REFERENCE LIST\s{0,}\n)", 
@@ -73,6 +45,7 @@ class PreRob():
         self.sent_path = sent_path
         self.txt_info = txt_info
         self.txt_paths = []
+        self.ids = []
         
     def get_txt_path(self):       
         txt_info = self.txt_info
@@ -96,6 +69,13 @@ class PreRob():
             for p in paths[:-1]:
                 txt_paths.append(p+'.txt')
             txt_paths.append(paths[-1])   
+            
+        # If 'txt_info' is the path of a csv containing relative paths
+        if os.path.exists(txt_info) == True and txt_info.endswith(".csv") == True:
+            path_df = pd.read_csv(txt_info, sep=',', engine="python")   
+            for i, row in path_df.iterrows():     
+                txt_paths.append(path_df.loc[i,'path'])          
+                self.ids.append(path_df.loc[i,'id'])
             
         self.txt_paths = txt_paths
     
@@ -135,41 +115,44 @@ class PreRob():
             output = {"message": "Folder/TXTs not found"}  # folder doesn't exist or no txt files found in the folder
             
         else:    
-            output = []
-            Id = 0
+            output = []            
             for path in self.txt_paths:
-                try:
-                    if os.path.isabs(path) == False:
-                        path = os.path.join(os.getcwd(), path)
-                    with open(path, 'r', encoding='utf-8', errors='ignore') as fin:
-                        text = fin.read()           
-                    text = self.process_text(text)   
-                    
-                    pr = pred_prob(self.prob_path['arg-r'], self.prob_path['fld-r'], self.prob_path['pth-r'], text).astype(float)
-                    pb = pred_prob(self.prob_path['arg-b'], self.prob_path['fld-b'], self.prob_path['pth-b'], text).astype(float)
-                    pi = pred_prob(self.prob_path['arg-i'], self.prob_path['fld-i'], self.prob_path['pth-i'], text).astype(float)
-                    pw = pred_prob_bert(self.prob_path['arg-w'], self.prob_path['pth-w'], text).astype(float)
-                    pe = pred_prob(self.prob_path['arg-e'], self.prob_path['fld-e'], self.prob_path['pth-e'], text).astype(float)  
-                    print('done')
-                    score = {"txt_id": Id, "txt_path": path,
-                			 "random": pr,
-                			 "blind": pb,
-                			 "interest": pi,
-                			 "welfare": pw,
-                			 "exclusion": pe}
-                    
-                    if num_sents > 0: 
-                        sr = extract_sents(self.sent_path['arg-r'], self.sent_path['fld-r'], self.sent_path['pth-r'], text, num_sents)
-                        sb = extract_sents(self.sent_path['arg-b'], self.sent_path['fld-b'], self.sent_path['pth-b'], text, num_sents)
-                        si = extract_sents(self.sent_path['arg-i'], self.sent_path['fld-i'], self.sent_path['pth-i'], text, num_sents)
-                        sw = extract_sents(self.sent_path['arg-w'], self.sent_path['fld-w'], self.sent_path['pth-w'], text, num_sents)
-                        se = extract_sents(self.sent_path['arg-e'], self.sent_path['fld-e'], self.sent_path['pth-e'], text, num_sents)
-                        score['sentences'] = {"random": sr, "blind": sb, "interest": si, "welfare": sw, "exclusion": se}             
-                    
-                except: 
-                    score = {"txt_id": Id, "txt_path": path, "message": "Path not found"}                    
-                Id = Id + 1
+
+                if os.path.isabs(path) == False:
+                    path = os.path.join(os.getcwd(), path)
+                with open(path, 'r', encoding='utf-8', errors='ignore') as fin:
+                    text = fin.read()           
+                text = self.process_text(text)   
+                
+                pr = pred_prob(self.prob_path['arg-r'], self.prob_path['fld-r'], self.prob_path['pth-r'], text).astype(float)
+                pb = pred_prob(self.prob_path['arg-b'], self.prob_path['fld-b'], self.prob_path['pth-b'], text).astype(float)
+                pi = pred_prob(self.prob_path['arg-i'], self.prob_path['fld-i'], self.prob_path['pth-i'], text).astype(float)
+                pw = pred_prob_distil(self.prob_path['arg-w'], self.prob_path['pth-w'], text).astype(float)
+                pe = pred_prob(self.prob_path['arg-e'], self.prob_path['fld-e'], self.prob_path['pth-e'], text).astype(float)  
+                print('done')
+                score = {"txt_path": path,
+            			 "random": pr,
+            			 "blind": pb,
+            			 "interest": pi,
+            			 "welfare": pw,
+            			 "exclusion": pe}
+                
+                if num_sents > 0: 
+                    sr = extract_sents(self.sent_path['arg-r'], self.sent_path['fld-r'], self.sent_path['pth-r'], text, num_sents)
+                    sb = extract_sents(self.sent_path['arg-b'], self.sent_path['fld-b'], self.sent_path['pth-b'], text, num_sents)
+                    si = extract_sents(self.sent_path['arg-i'], self.sent_path['fld-i'], self.sent_path['pth-i'], text, num_sents)
+                    sw = extract_sents(self.sent_path['arg-w'], self.sent_path['fld-w'], self.sent_path['pth-w'], text, num_sents)
+                    se = extract_sents(self.sent_path['arg-e'], self.sent_path['fld-e'], self.sent_path['pth-e'], text, num_sents)
+                    score['sentences'] = {"random": sr, "blind": sb, "interest": si, "welfare": sw, "exclusion": se}             
+                              
                 output.append(score)
+                
+            for i, _ in enumerate(output):
+                if self.ids is None:
+                    output[i]['id'] = str(i + 1)
+                else:
+                    output[i]['id'] = str(self.ids[i])
+                               
         return output
 
 
@@ -189,7 +172,12 @@ class ROB(Resource):
         
         if (len(request.form)) == 1:
             output = rober.pred_probs()
-    
+            # If input is a csv, output csv
+            if txt_info.endswith(".csv") == True:
+                output_df = pd.DataFrame(output)
+                csv_dir = os.path.dirname(txt_info)
+                output_df.to_csv(os.path.join(csv_dir, 'output.csv'), sep=',', encoding='utf-8')
+                  
         if (len(request.form)) == 2:
             try:
                 json_path = request.form['out']
