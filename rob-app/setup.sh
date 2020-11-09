e15b19c8f1e5ce8e7baf3dcc691b25d25802015d
Qianying Wang <w.qianying@outlook.com>
2020-11-09 19:32:37 +0000

    dsc_w path

--- rob-app/setup.sh
+++ rob-app/setup.sh
@@ -10,6 +10,6 @@ python -m spacy download en_core_web_sm
 
 echo "====================================================="
 echo "Loading pre-trained weights.."
-curl -c /tmp/cookies "https://drive.google.com/uc?export=download&id=1vztBL9WwUh1vjXOjSfPVgnUTpqdFEcKY" > /tmp/intermezzo.html
-curl -L -b /tmp/cookies "https://drive.google.com$(cat /tmp/intermezzo.html | grep -Po 'uc-download-link" [^>]* href="\K[^"]*' | sed 's/\&amp;/\&/g')" > pth/bert_w0.pth.tar
+curl -c /tmp/cookies "https://drive.google.com/uc?export=download&id=18YixZQ4otcZWdAMavy5OviR0579kWrCm" > /tmp/intermezzo.html
+curl -L -b /tmp/cookies "https://drive.google.com$(cat /tmp/intermezzo.html | grep -Po 'uc-download-link" [^>]* href="\K[^"]*' | sed 's/\&amp;/\&/g')" > pth/dsc_w0.pth.tar
 echo "Finished."
